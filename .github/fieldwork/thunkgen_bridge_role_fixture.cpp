af59c50c5434a57c77836fba3661cd9c5963eb46
Leo <cheerleaderleo@outlook.com>
2026-08-14 17:57:28 +0800

    Use realistic callback and indirect overlap in thunkgen role fixture

--- .github/fieldwork/thunkgen_bridge_role_fixture.cpp
+++ .github/fieldwork/thunkgen_bridge_role_fixture.cpp
@@ -8,11 +8,26 @@ struct fex_gen_config {
 template<typename>
 struct fex_gen_type {};
 
-using FixtureCallback = int (*)(int);
-int fixture_register_callback(FixtureCallback callback, int value);
+// Unpacker-only registration: ordinary callback parameter with no matching
+// indirect-call signature.
+using FixtureCallbackOnly = void (*)(int);
+int fixture_register_callback_only(FixtureCallbackOnly callback);
 template<>
-struct fex_gen_config<fixture_register_callback> {};
+struct fex_gen_config<fixture_register_callback_only> {};
 
-using FixtureExplicitBridge = long (*)(long);
+// This callback signature also appears as an indirect API function below.
+// Canonical-signature deduplication must OR the two registrations to produce
+// caller=1, unpacker=1.
+using FixtureSharedCallback = int (*)(int);
+int fixture_register_shared_callback(FixtureSharedCallback callback, int value);
 template<>
-struct fex_gen_type<FixtureExplicitBridge> {};
+struct fex_gen_config<fixture_register_shared_callback> {};
+
+int fixture_indirect(int value);
+namespace internal {
+template<auto>
+struct fex_gen_config : fexgen::generate_guest_symtable, fexgen::indirect_guest_calls {};
+
+template<>
+struct fex_gen_config<fixture_indirect> {};
+} // namespace internal
