4e311ab73b7dfb26d5fc37701b46406c9ff2588b
Leo <cheerleaderleo@outlook.com>
2026-08-14 15:45:06 +0800

    Mirror Vulkan allocator pointer mix in generator test

--- LINUX_FIELDWORK_CUSTOM_REPACK_FUNCPTR_TEST.py
+++ LINUX_FIELDWORK_CUSTOM_REPACK_FUNCPTR_TEST.py
@@ -22,5 +22,28 @@ TEST_CASE_METHOD(Fixture, "Custom repack function-pointer member") {
   CHECK_NOTHROW(run_thunkgen_host(prelude, code, guest_abi));
 }
 
+TEST_CASE_METHOD(Fixture, "Custom repack allocator-like pointer members") {
+  auto guest_abi = GENERATE(GuestABI::X86_32, GuestABI::X86_64);
+  INFO(guest_abi);
+
+  const char* prelude = "using cb0 = void* (*)(void*, unsigned long, unsigned long, unsigned);\n"
+                        "using cb1 = void* (*)(void*, void*, unsigned long, unsigned long, unsigned);\n"
+                        "using cb2 = void (*)(void*, void*);\n"
+                        "using cb3 = void (*)(void*, unsigned long, unsigned, unsigned);\n"
+                        "struct A { void* userdata; cb0 alloc; cb1 realloc; cb2 free; cb3 ia; cb3 iff; };\n";
+  const char* code = "#include <thunks_common.h>\n"
+                     "void func(const A*);\n"
+                     "template<auto> struct fex_gen_config {};\n"
+                     "template<> struct fex_gen_config<&A::userdata> : fexgen::custom_repack {};\n"
+                     "template<> struct fex_gen_config<&A::alloc> : fexgen::custom_repack {};\n"
+                     "template<> struct fex_gen_config<&A::realloc> : fexgen::custom_repack {};\n"
+                     "template<> struct fex_gen_config<&A::free> : fexgen::custom_repack {};\n"
+                     "template<> struct fex_gen_config<&A::ia> : fexgen::custom_repack {};\n"
+                     "template<> struct fex_gen_config<&A::iff> : fexgen::custom_repack {};\n"
+                     "template<> struct fex_gen_config<func> {};\n";
+
+  CHECK_NOTHROW(run_thunkgen_host(prelude, code, guest_abi));
+}
+
 '''
 path.write_text(text.replace(anchor, test + anchor, 1))
